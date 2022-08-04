c3aaa9df673c5d1ea97a94af0436066bd2a0a206
AliceKoh <109507439+AliceKoh@users.noreply.github.com>
2022-08-04 13:20:12 +0800

    Update quest_app.py

--- quest_app.py
+++ quest_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 from PIL import Image
-#import streamlit.components.v1 as components
 import pandas as pd
-#import numpy as np
+import numpy as np
 from prognosis_app import run_prognosis_app
 
 def run_quest_app():
